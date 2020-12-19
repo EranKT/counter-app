3695d9e3830c61d903ee7f686d2dfdff3092bb5f
EranKT <eran@ktfgalaxy.com>
2020-12-19 11:26:45 +0100

    refactor

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 
 // Stateless Functional Component
-const NavBar = () => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <a className="navbar-brand" href="#">
           Navbar{" "}
           <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
+            {totalCounters}
           </span>
         </a>
       </div>
